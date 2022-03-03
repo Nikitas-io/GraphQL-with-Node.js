78a8424977bbe22a530f4ac88187b3c38ef0fe19
Mona Lisa <chrononautic@gmail.com>
2022-03-04 01:02:26 +0200

    Added a review rating filter for the products

--- resolvers/Category.js
+++ resolvers/Category.js
@@ -1,20 +1,50 @@
+const { reviews } = require("../db");
+
 exports.Category = {
     // Populate the products array.
-    products: ({id: categoryId}, {filter}, {products}) => {
+    products: ({id: categoryId}, {filter}, {products, reviews}) => {
         // Filter through the products and return only the products of the category.
         const categoryProducts = products.filter((product) => product.categoryId === categoryId);
 
         let filteredCategoryProducts = categoryProducts;
         // Check if there's a filter param passed to the resolver.
         if(filter) {
+            const {onSale, avgRating} = filter;
             // Check if the filter's onSale attribute is true.
-            if(filter.onSale) {
+            if(onSale) {
+                // Get the products that are on sale.
                 filteredCategoryProducts = filteredCategoryProducts.filter((product) => {
                     // Return only the products that are on sale.
                     return product.onSale
                 });
             }
+
+            // Check if the given average rating is within the allowed ratings.
+            if([1,2,3,4,5].includes(avgRating)) {
+                // Get the products that are equal to or above the average rating.
+                filteredCategoryProducts = filteredCategoryProducts.filter((product) => {
+                    // Initialize the sum of the ratings for a specific product. 
+                    let sumRating = 0;
+                    let numberOfReviews = 0;
+                    // Loop through the reviews.
+                    reviews.forEach((review) => {
+                        // Check if the review belongs to the product.
+                        if(review.productId === product.id) {
+                            // Sum up the reviews rating.
+                            sumRating += review.rating;
+                            // Increment the number of reviews.
+                            numberOfReviews++;
+                        }
+                    });
+
+                    // Calculate the average rating for the product.
+                    const avgProductRating = sumRating / numberOfReviews;
+                    // Return the products with a rating greater or equal to the average rating.
+                    return avgProductRating >= avgRating;
+                });
+            }
         }
+        console.log('The filtered products: ', filteredCategoryProducts)
 
         return filteredCategoryProducts;
     }
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,17 +1,45 @@
 exports.Query = {
-    products: (parent, {filter}, {products}) => {
+    products: (parent, {filter}, {products, reviews}) => {
         let filteredProducts = products;
+        const {onSale, avgRating} = filter;
 
         // Check if there's a filter param passed to the resolver.
         if(filter) {
             // Check if the filter's onSale attribute is true.
-            if(filter.onSale) {
+            if(onSale) {
+                // Get the products that are on sale.
                 filteredProducts = filteredProducts.filter((product) => {
                     // Return ONLY the products that are on sale.
                     return product.onSale
                 });
             }
         }
+
+        // Check if the given average rating is within the allowed ratings.
+        if([1,2,3,4,5].includes(avgRating)) {
+            // Get the products that are equal to or above the average rating.
+            filteredProducts = filteredProducts.filter((product) => {
+                // Initialize the sum of the ratings for a specific product. 
+                let sumRating = 0;
+                let numberOfReviews = 0;
+                // Loop through the reviews.
+                reviews.forEach((review) => {
+                    // Check if the review belongs to the product.
+                    if(review.productId === product.id) {
+                        // Sum up the reviews rating.
+                        sumRating += review.rating;
+                        // Increment the number of reviews.
+                        numberOfReviews++;
+                    }
+                });
+
+                // Calculate the average rating for the product.
+                const avgProductRating = sumRating / numberOfReviews;
+                // Return the products with a rating greater or equal to the average rating.
+                return avgProductRating >= avgRating;
+            });
+        }
+        // Return the filtered products.
         return filteredProducts;
     },
     product: (parent, {id}, {products}) => {
